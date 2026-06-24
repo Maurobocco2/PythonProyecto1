7922a3a3f0c1ebc49b1609872c268f3e9158b8dc
Mauro Boccolini <mauroboccolini.20@gmail.com>
2026-06-23 21:13:32 -0300

    Creacion de vistas y Admin User

--- Proyecto1/myapp/admin.py
+++ Proyecto1/myapp/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from .models import Cliente, Equipo, Tecnico, Reparacion
 
 
-admin.site.site_header = "Admin de ServisPC"
-admin.site.site_title = "Panel de Administración de ServisPC"
-admin.site.index_title = "Bienvenido al Panel de Administración de ServisPC"
+admin.site.site_header = "Admin de NovaRepara"
+admin.site.site_title = "Panel de Administración de NovaRepara"
+admin.site.index_title = "Bienvenido al Panel de Administración de NovaRepara"
 admin.site.register(Cliente)
 admin.site.register(Equipo)
 admin.site.register(Tecnico)
--- Proyecto1/myapp/views.py
+++ Proyecto1/myapp/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    context = {"mensaje": "Bienvenidos a mi app Django"}
+    context = {"mensaje": "Ofrecemos servicios de reparación de computadoras, mantenimiento y soporte técnico."}
     return render(request, "myapp/index.html", context)
